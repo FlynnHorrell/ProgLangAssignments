8a2652c480142c009a88d20e86bdb4da244b7254
Flynn Horrell <horrellf16@hanover.edu>
2016-02-04 19:32:17 -0500

    Implemented and tested  result, is_tie, and to_f.

--- assignment3tests.ml
+++ assignment3tests.ml
@@ -1,6 +1,18 @@
 let t1a = result (Rock, Paper) = SndWin
+let t1b = result (Rock, Rock) = Tie
+let t1c = result (Rock, Scissors) = FstWin
+let t1d = result (Paper, Paper) = Tie
+let t1e = result (Paper, Scissors) = SndWin
+let t1f = result (Paper, Rock) = FstWin
+let t1g = result (Scissors, Paper) = FstWin
+let t1h = result (Scissors, Scissors) = Tie
+let t1i = result (Scissors, Rock) = SndWin
 
 let t2a = is_tie (Rock, Paper) = false
+let t2b = is_tie (Rock, Rock) = true
+let t2c = is_tie (Paper, Paper) = true
+let t2d = is_tie (Scissors, Scissors) = true
+let t2e = is_tie (Scissors, Paper) = false
 
 let t3a = game_from_plays ([Rock; Paper; Rock], [Scissors; Rock; Rock]) =
                [(Rock, Scissors); (Paper, Rock); (Rock, Rock)]
@@ -10,6 +22,9 @@ let t4a = valid_game [(Rock, Scissors)] = true
 let t5a = play_game [(Rock, Rock); (Scissors, Rock)] = SndWin
 
 let t6a = to_f (F 2.3) = 2.3
+let t6b = to_f (F 0.0) = 0.0
+let t6c = to_f (C 0.0) = 32.0
+let t6d = to_f (C 100.0) = 212.0
 
 let t7a = temp_compare (F 2.3, F 4.5) = -1
 
