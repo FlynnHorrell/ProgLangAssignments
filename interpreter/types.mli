63856cd3da363064c95cdb69796f782508dbd197
Flynn Horrell <horrellf16@hanover.edu>
2016-03-11 16:30:12 -0500

    Tested BoolC

---
+++ interpreter/types.mli
@@ -0,0 +1,24 @@
+exception Desugar of string      (* Use for desugarer errors *)
+exception Interp of string       (* Use for interpreter errors *)
+
+type exprS = NumS of float
+
+type exprC = NumC of float
+			|BoolC of bool
+
+type value = Num of float
+			 |Bool of bool
+
+(* Environment lookup *)
+type 'a env
+val empty : 'a env
+val lookup : string -> 'a env -> 'a option
+val bind :  string -> 'a -> 'a env -> 'a env
+
+(* Interpreter steps *)
+val desugar : exprS -> exprC
+val interp : value env -> exprC -> value
+val evaluate : exprC -> value
+
+(* result post-processing *)
+val valToString : value -> string
